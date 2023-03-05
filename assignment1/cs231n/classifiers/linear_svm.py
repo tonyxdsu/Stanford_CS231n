2992ee6f9a3d1637867732e26de3f6647c930261
tonyxdsu <tonyxdsu@gmail.com>
2023-03-05 01:49:20 -0800

    svm loss and gradient vectorized

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -31,30 +31,45 @@ def svm_loss_naive(W, X, y, reg):
     for i in range(num_train):
         scores = X[i].dot(W)
         correct_class_score = scores[y[i]]
+        # compute L_i
+        num_exceeds_margin = 0
         for j in range(num_classes):
             if j == y[i]:
                 continue
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
+                # add computed grad for jth col of dW
+                dW[:,j] += X[i,:]
+                # count num misclassified for j == y[ith] col of grad
+                num_exceeds_margin += 1
+        # add computed gradient for j == y[ith] col of dW
+        dW[:,y[i]] += -num_exceeds_margin * X[i,:]
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
     loss /= num_train
 
+    # normalize the batch gradient
+    dW /= num_train
+
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
+    
+    # add regularization's contribution to the gradient
+    dW += reg * 2 * W;
 
     #############################################################################
     # TODO:                                                                     #
     # Compute the gradient of the loss function and store it dW.                #
-    # Rather that first computing the loss and then computing the derivative,   #
+    # Rather than first computing the loss and then computing the derivative,   #
     # it may be simpler to compute the derivative at the same time that the     #
     # loss is being computed. As a result you may need to modify some of the    #
     # code above to compute the gradient.                                       #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
+    # above
     pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -66,7 +81,19 @@ def svm_loss_vectorized(W, X, y, reg):
     """
     Structured SVM loss function, vectorized implementation.
 
-    Inputs and outputs are the same as svm_loss_naive.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
+
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
+
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
     """
     loss = 0.0
     dW = np.zeros(W.shape)  # initialize the gradient as zero
@@ -78,7 +105,25 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+
+    # scores: shape (N, C)
+    scores = np.matmul(X, W)
+    
+    # correct_class_scores: shape (N,)
+    correct_class_scores = scores[np.arange(num_train), y]
+
+    # margins: shape (N, C)
+    # correct_class_scores[:, np.newaxis]: shape [N, 1]
+    margins = np.maximum(0, scores - correct_class_scores[:, np.newaxis] + 1) # note delta = 1
+
+    # set correct class margin to 0
+    margins[np.arange(num_train), y] = 0
+
+    loss = np.sum(margins)
+    loss /= num_train
+
+    loss += reg * np.sum(W * W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -93,7 +138,20 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    # exceeds_margin: shape (N, C)
+    exceeds_margin = margins
+    exceeds_margin[margins > 0] = 1
+
+    # num_exceeds_margin: shape (N,)
+    num_exceeds_margin = np.sum(exceeds_margin, axis = 1)
+
+    # set element of the correct class to -total number of classes that exceed margin
+    # for use in matrix multiplication for gradient
+    exceeds_margin[np.arange(num_train), y] = -num_exceeds_margin
+
+    dW = np.matmul(X.T, exceeds_margin)
+    dW /= num_train
+    dW += reg * 2 * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
